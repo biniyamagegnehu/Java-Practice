7cede183065ed1d48c5882276a4b6998525fc880
biniyamagegnehu <biniyamagegnehu2@gmail.com>
2025-04-03 09:38:54 +0300

    a default constructor example

---
+++ DefaultConstructor.java
@@ -0,0 +1,13 @@
+class Cat {
+    String name;
+    int age;
+
+}
+
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        Cat cat = new Cat(); 
+        System.out.println(cat.name);
+        System.out.println(cat.age);
+    }
+}
